aff5285ce0417c70d8526a525dc5e2eed01ffb4c
kirypto <kirypto@gmail.com>
2018-05-04 22:44:44 -0600

    Added a PlayerWeaponConeScript

---
+++ ProjectSource/Assets/Scripts/PlayerWeaponConeScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerWeaponConeScript : MonoBehaviour
+{
+    private Transform _weaponCone;
+    private Transform _weaponConeTriangleComponent;
+    private Transform _weaponConeOvalComponent;
+
+    private void Awake()
+    {
+        _weaponCone = transform.Find("WeaponCone");
+        _weaponConeTriangleComponent = _weaponCone.Find("TriangleComponent");
+        _weaponConeOvalComponent = _weaponCone.Find("OvalComponent");
+
+        print(_weaponCone);
+        print(_weaponConeTriangleComponent);
+        print(_weaponConeOvalComponent);
+    }
+}

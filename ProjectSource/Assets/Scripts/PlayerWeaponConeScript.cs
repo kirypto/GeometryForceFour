b4d52789b9ce3d1d24b8dd23e9cda597fe6fac99
kirypto <kirypto@gmail.com>
2018-05-05 11:49:41 -0600

    Added collider data to PlayerWeaponConeScript

--- ProjectSource/Assets/Scripts/PlayerWeaponConeScript.cs
+++ ProjectSource/Assets/Scripts/PlayerWeaponConeScript.cs
@@ -3,13 +3,25 @@
 public class PlayerWeaponConeScript : MonoBehaviour
 {
     private Transform _weaponCone;
-    private Transform _weaponConeTriangleComponent;
-    private Transform _weaponConeOvalComponent;
+    private Transform _triangleTransform;
+    private Transform _ovalTransform;
+
+    private Collider2D _triangleCollider;
+    private Collider2D _ovalCollider;
+
 
     private void Awake()
     {
         _weaponCone = transform.Find("WeaponCone");
-        _weaponConeTriangleComponent = _weaponCone.Find("TriangleComponent");
-        _weaponConeOvalComponent = _weaponCone.Find("OvalComponent");
+        _triangleTransform = _weaponCone.Find("TriangleComponent");
+        _ovalTransform = _weaponCone.Find("OvalComponent");
+
+        _triangleCollider = _triangleTransform.GetComponent<PolygonCollider2D>();
+        _ovalCollider = _ovalTransform.GetComponent<CapsuleCollider2D>();
+
+        _triangleCollider.enabled = false;
+        _ovalCollider.enabled = false;
     }
+
+
 }

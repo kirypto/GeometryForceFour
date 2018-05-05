d21f6a088b68df2644a4dd0354f17fe5f7918962
kirypto <kirypto@gmail.com>
2018-05-04 21:38:40 -0600

    Created a player movement script

---
+++ ProjectSource/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+
+    [SerializeField] private float _movementForce = 1f;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _rigidbody.AddForce(Vector2.up * _movementForce);
+        }
+        else if (Input.GetKey(KeyCode.A)) {
+            _rigidbody.AddForce(Vector2.left * _movementForce);
+        }
+        else if (Input.GetKey(KeyCode.S)) {
+            _rigidbody.AddForce(Vector2.down * _movementForce);
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            _rigidbody.AddForce(Vector2.right * _movementForce);
+        }
+    }
+}

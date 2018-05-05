151398388c5c0cafc3181f6ee40828a89c2c4b14
Kirk <kirkdmcm@gmail.com>
2018-05-05 11:56:41 -0600

    set up structs for ai mobs

---
+++ ProjectSource/Assets/Scripts/AIComponentData.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public struct MobComponentData
+{
+    public Vector3 Position;
+    public Vector3 Velocity;
+}
+      
+
+public struct CenterMassJobInput
+{
+    // target center of group
+    public float GroupCenterMassScale;
+}
+public struct CenterMassJobOutput
+{
+    // target center of group
+    public Vector3 GroupCenterMass;
+}
+
+
+public struct EqualizeSpeedJobInput
+{
+    // match speed
+    public float EqualizeSpeedScale;
+    public float EqualizeSpeedRadius;
+}
+public struct EqualizeSpeedJobOutput
+{
+    // match speed
+    public Vector3 EqualizeSpeed;
+}
+
+
+public struct PersonalSpaceJobInput
+{
+    // personal space
+    public float PersonalSpaceScale;
+    public float PersonalSpaceRadius;
+}
+public struct PersonalSpaceJobOutput
+{
+    // personal space
+    public Vector3 PersonalSpace;
+}
---
+++ ProjectSource/Assets/Scripts/AIDirectorScript.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Collections;
+using UnityEngine;
+
+public class AIDirectorScript : MonoBehaviour
+{
+    [SerializeField] private bool CenterMassToggle = true;
+    [SerializeField] private bool EqualizeSpeedToggle = true;
+    [SerializeField] private bool PersonalSpaceToggle = true;
+
+    private NativeArray<MobComponentData> allMobs;
+    private NativeArray<CenterMassJobInput> centerMassInputs;
+    private NativeArray<CenterMassJobOutput> centerMassOutputs;
+    private NativeArray<EqualizeSpeedJobInput> equalizeSpeedInputs;
+    private NativeArray<EqualizeSpeedJobOutput> equalizeSpeedOutputs;
+    private NativeArray<PersonalSpaceJobInput> personalSpaceInputs;
+    private NativeArray<PersonalSpaceJobOutput> personalSpaceOutputs;
+
+}

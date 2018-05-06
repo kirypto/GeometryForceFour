8a8d13ff01bab3ec7a64fee9754183499c02fa34
kirypto <kirypto@gmail.com>
2018-05-06 16:45:16 -0600

    Made totems come back into play area

--- ProjectSource/Assets/Scripts/TotemScript.cs
+++ ProjectSource/Assets/Scripts/TotemScript.cs
@@ -3,4 +3,23 @@
 public class TotemScript : MonoBehaviour
 {
     public bool ShouldBlink;
+
+    [SerializeField] private float _xPosMax = 10f;
+    [SerializeField] private float _xPosMin = -10f;
+    [SerializeField] private float _yPosMax = 10f;
+    [SerializeField] private float _yPosMin = -10f;
+
+    [SerializeField] private float _reboundForce = 1f;
+
+    private void Update()
+    {
+        if (transform.position.x > _xPosMax ||
+            transform.position.y > _yPosMax ||
+            transform.position.x < _xPosMin ||
+            transform.position.y < _yPosMin)
+        {
+            Vector2 vector2 = Vector2.zero - (Vector2) transform.position;
+            GetComponent<Rigidbody2D>().AddForce(vector2 * _reboundForce);
+        }
+    }
 }
